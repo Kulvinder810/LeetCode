7a2913efb96e95d06fc80e645833cdbbf3b99e71
Kulvinder <kuls810@gmail.com>
2022-08-18 22:04:15 +0530

    two sum added

---
+++ 1.cpp
@@ -0,0 +1,12 @@
+//Two sum
+vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> mp;
+        mp[nums[0]]=0;
+        for(int i=1;i<nums.size();i++){
+            if(mp.find(target-nums[i])!=mp.end()){
+                return vector<int> {mp[target-nums[i]],i};
+            }
+            mp[nums[i]]=i;
+        }
+        return vector<int> {-1,-1};
+    }
